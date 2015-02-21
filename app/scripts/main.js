8ffc98f96719fa774a8ed3030354c27d9614c522
S3ak <nsineke@gmail.com>
2015-02-21 20:25:25 +0200

    added off-canvas menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,12 @@
 $(function(){
+  'use strict';
   var menuMorph = function() {
     $('.icon-menu_atom--top').toggleClass('icon-menu_atom--rotatedown');
     $('.icon-menu_atom--middle').toggleClass('icon-menu_atom--hide-middle');
     $('.icon-menu_atom--bottom').toggleClass('icon-menu_atom--rotateup');
   };
 
-  $('#js-nav-trigger').on('click', function() {
+  $('#js-nav-trigger, .site-overlay').on('click', function() {
     menuMorph();
   });
 });
