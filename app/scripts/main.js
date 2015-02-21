517db75840faf7dae8702bf10d2fddef8ce04313
S3ak <nsineke@gmail.com>
2015-02-21 19:17:55 +0200

    initial layout made

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+  var menuMorph = function() {
+    $('.icon-menu_atom--top').toggleClass('icon-menu_atom--rotatedown');
+    $('.icon-menu_atom--middle').toggleClass('icon-menu_atom--hide-middle');
+    $('.icon-menu_atom--bottom').toggleClass('icon-menu_atom--rotateup');
+  };
+
+  $('#js-nav-trigger').on('click', function() {
+    menuMorph();
+  });
+});
